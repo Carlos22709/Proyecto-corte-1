4a59517ce39c969df9c6c44c98ec36163836b54d
LangeFR <mdjvanegasc@gmail.com>
2024-02-21 22:22:33 -0500

    Big changes en apariencia

--- Archivos/script.js
+++ Archivos/script.js
@@ -3,7 +3,7 @@
     Landing Page Inicio
     -------------------
 */
-function readMorePresentacion() {
+/*function readMorePresentacion() {
     var dots = document.getElementById("dotsReadMorePresentacion");
     var moreText = document.getElementById("moreTextPresentacion");
     var btnText = document.getElementById("btnReadMorePresentacion");
@@ -17,6 +17,42 @@ function readMorePresentacion() {
       btnText.innerHTML = "Read less";
       moreText.style.display = "inline";
     }
+}*/
+
+function readMorePresentacion() {
+  var dots = document.getElementById("dotsReadMorePresentacion");
+  var moreText = document.getElementById("moreTextPresentacion");
+  var btnText = document.getElementById("btnReadMorePresentacion");
+  var hamburguesaImg = document.getElementById("hamburguesaDescripcion");
+  var malteadaImg = document.getElementById("malteadaDescripcion");
+  var descripcion = document.getElementsByClassName("divPresentacion").clientHeight;
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+
+      hamburguesaImg.style.marginLeft = "5px"; // Restablecer margen izquierdo
+        hamburguesaImg.setAttribute("style", "aspect-ratio: 150 / 100; min-height: 150px; min-width: 225px; max-width: 225px; margin-top: 5px; max-height: 150px;");
+        malteadaImg.setAttribute("style", "aspect-ratio: 150 / 100; min-height: 150px; min-width: 225px; max-width: 225px; margin-top: 5px; max-height: 150px;");
+
+
+
+
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+      hamburguesaImg.style.aspectRatio = 225 / 350; // Alter
+      hamburguesaImg.style.minHeight = "350px"; // Altura automática para mostrar la imagen completa
+      hamburguesaImg.style.minWidth = "225px"; // Ancho automático para mantener el aspect ratio
+      hamburguesaImg.style.maxWidth = "225px"; // Ancho automático para mantener el aspect ratio
+
+      malteadaImg.style.aspectRatio = 225 / 350; // Alter
+      malteadaImg.style.minHeight = "350px"; // Ancho gener
+      malteadaImg.style.minWidth = "225px"; // Ancho automático para mantener el aspect ratio
+      malteadaImg.style.maxWidth = "225px"; // Ancho automático para mantener el aspect ratio
+  }
 }
 
 
